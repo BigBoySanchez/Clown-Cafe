60f05ed2c49d99edb31b4e94a434d572ba7b35ea
BigBoySanchez <jacobmath3@gmail.com>
2024-08-23 21:07:14 -0700

    added plate

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -6,8 +6,20 @@ export class Preloader extends Scene {
   }
 
   init() {
-    const {WIDTH, HEIGHT} = this.sys.game.canvas;
+    const WIDTH = this.game.scale.width;
+    const HEIGHT = this.game.scale.height;
 
-    this.add.rectangle()
+    this.add.rectangle(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 128, 0x999999);
+    const bar = this.add.rectangle(WIDTH / 4, HEIGHT / 2, 0, HEIGHT / 32, 0xffffff)
+
+    this.load.on('progress', (progress) => {
+      bar.width = (WIDTH / 2) * progress;
+    });
+  }
+
+  preload() {
+    this.load.setPath('/assets');
+
+    this.load.image('plate', 'plate.png');
   }
 }
